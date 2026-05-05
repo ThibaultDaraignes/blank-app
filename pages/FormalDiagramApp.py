bfeb615ba3ce848639f9a70df88a663e0fa23c51
ThibaultDaraignes <tdaraignes@gmail.com>
2026-05-05 17:19:26 +0200

    Update FormalDiagramApp.py

--- pages/FormalDiagramApp.py
+++ pages/FormalDiagramApp.py
@@ -199,7 +199,7 @@ if audio_file:
     #display waweform
     fig, ax = plt.subplots()
     librosa.display.waveshow(wave2, sr=sr, max_points=20000, axis='m')
-    st.pyplot(fig, width="content")
+    st.pyplot(fig, width=600*200)
 
     print(start, duration, end)
 
