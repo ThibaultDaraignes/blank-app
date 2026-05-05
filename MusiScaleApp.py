ae5db128216ae83e6d7ae2aac4990a983ae07142
ThibaultDaraignes <tdaraignes@gmail.com>
2026-05-05 18:28:01 +0200

    Update MusiScaleApp.py

--- MusiScaleApp.py
+++ MusiScaleApp.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 # # Define the pages
-main_page = st.Page("pages/Welcome.py",title="Welcome", default=True)
+main_page = st.Page("pages/Welcome.py",title="Home", default=True)
 page_1 = st.Page("pages/FormalDiagramApp.py", title="Formal Diagram")
 page_2 = st.Page("pages/TimeDurationDiagram.py", title="Time-Duration Diagram")
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
